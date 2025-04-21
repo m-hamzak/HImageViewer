89f24751f345e257fd506a12a464f5a0fb9d393f
Muhammad Hamza Khalid <46857881+m-hamzak@users.noreply.github.com>
2025-04-21 19:38:42 +0500

    Add files via upload

---
+++ HImageViewer/HImageViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HImageViewerApp.swift
+//  HImageViewer
+//
+//  Created by Hamza Khalid on 21/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct HImageViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
