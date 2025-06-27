b4171e1038dd1bd8335727457f8e530f2c6745b8
m-hamzak <m_hamzak1997@yahoo.com>
2025-06-27 17:37:04 +0500

    fixed issue for centring single image

--- Sources/HImageViewer/PhotoView.swift
+++ Sources/HImageViewer/PhotoView.swift
@@ -13,30 +13,36 @@ public struct PhotoView: View {
     @State private var didFailToLoad: Bool = false
     let photo: PhotoAsset
     let isSinglePhotoMode: Bool
-
+    
     public var body: some View {
-        Group {
-            if let image {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFill()
-            } else if didFailToLoad {
-                Color.red.opacity(0.2)
-                    .overlay(
-                        Image(systemName: "exclamationmark.triangle")
-                            .foregroundColor(.red)
-                            .font(.largeTitle)
-                    )
-            } else {
-                Color.gray.opacity(0.2)
-                    .overlay(
-                        ProgressView()
-                    )
+        GeometryReader { proxy in
+            VStack {
+                Spacer()
+                if let image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .clipped()
+                } else if didFailToLoad {
+                    Color.red.opacity(0.2)
+                        .overlay(
+                            Image(systemName: "exclamationmark.triangle")
+                                .foregroundColor(.red)
+                                .font(.largeTitle)
+                        )
+                } else {
+                    Color.gray.opacity(0.2)
+                        .overlay(
+                            ProgressView()
+                        )
+                }
+                Spacer()
             }
         }
         .onAppear {
             guard image == nil && !didFailToLoad else { return }
-
+            
             let completion: (UIImage?) -> Void = { img in
                 if let img = img {
                     self.image = img
@@ -44,7 +50,7 @@ public struct PhotoView: View {
                     self.didFailToLoad = true
                 }
             }
-
+            
             if isSinglePhotoMode {
                 photo.loadFullImage(completion: completion)
             } else {
